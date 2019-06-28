aceb2563a8db7c0889531e8305ecae44d8012430
onting <hemool123@naver.com>
2019-06-28 04:13:31 -0700

    Adjust ibus-hanjp structrue

--- ibus-hanjp/src/gstringcomp.h
+++
@@ -1,5 +0,0 @@
-#include <glib.h>
-#include <hangul.h>
-
-GString* g_string_append_ucs4(GString* str, const ucschar* s, gint len);
-gchar* ustring_to_utf8(const ucschar *src, gchar* dest, gint len);
